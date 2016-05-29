dbdb83d512349d0355a7ef66246ab01a3178d94b
artist <artist@studyGit.com>
2016-05-29 22:48:02 +0900

    init

--- WatchCommMixed WatchKit Extension/InterfaceController.swift
+++ WatchCommMixed WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,77 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+class InterfaceController: WKInterfaceController,WCSessionDelegate {
+    
+    // ------------------------------------ WCSession init !!!!
+    
+    let session:WCSession? = WCSession.isSupported() ? WCSession.defaultSession() : nil
+    
+    override init() {
+        super.init()
+        
+        configureWCSession()
+    }
+    
+    private func configureWCSession() {
+        session?.delegate = self;
+        session?.activateSession()
+    }
+    
+    
+    
+    // ------------------------------------ Context RX
+    
+    func session(session: WCSession, didReceiveApplicationContext applicationContext: [String:AnyObject]) {
+        
+        print("rx deleated called")
+        
+        let received = applicationContext["appData"] as? String
+        
+        dispatch_async(dispatch_get_main_queue()) { // for faster UI
+            if let received = received {
+                
+                print("data: \(received)")
+                self.contextLabel.setText("data: \(received)")
+            }
+        }
+        
+    }
 
-class InterfaceController: WKInterfaceController {
+    
+    
+ 
+    // ------------------------------------ Im RX
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
+        
+        dispatch_async(dispatch_get_main_queue()) { // for faster UI
 
+        if let counter = message["appCounter"] {
+            
+            print("app data : \(counter)")
+            
+            self.instantLabel.setText("ha:\(counter)")
+        }
+            
+        }
+    }
+
+    
+    
+    
+    
+    
+    @IBOutlet var instantLabel: WKInterfaceLabel!
+    
+    
+    @IBOutlet var contextLabel: WKInterfaceLabel!
+    
+    
+    
+ 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
--- WatchCommMixed/ViewController.swift
+++ WatchCommMixed/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , WCSessionDelegate{
+    
+    let session:WCSession? = WCSession.isSupported() ? WCSession.defaultSession() : nil
+    
+    private func configureWCSession(){
+        session?.delegate = self
+        session?.activateSession()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        configureWCSession()
+        
+    }
+    
+    
+    
+    
+    
+    // auto generated code by XCODE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target:self, selector:#selector(updateCounter), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,84 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    //CONTEXT
+    
+    func sendTextUsingContext(aString:String){
+        
+        let appDict = ["appData" : aString]
+        
+        do {
+            try session?.updateApplicationContext(appDict)
+            
+            print("item sent")
+        } catch{
+            print("err in sending conTexxT")
+        }
+    }
 
+    
+    //IM
+    
+    var counter : Int = 1
+    
+    func sendIMData (){
+        
+        
+        var appData = ["appCounter": counter ]
+        appData   ["appTime"] =  36
+        
+        counter += 1
+        
+        if let session = session where session.reachable{
+            session.sendMessage(appData, replyHandler: { replyData in
+                
+                print(replyData)
+                
+                }, errorHandler: { error in
+                    
+                    print(error)
+            })
+        }else{
+            print("bt erroR!!")
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    //IBOutlets
+    
+    @IBAction func sendIM(sender: AnyObject) {
+        sendIMData()
+    }
+    
+    
+    @IBAction func sendContextOne(sender: AnyObject) {
+        sendTextUsingContext("earth")
+    }
+    
+    @IBAction func sendContextTwo(sender: AnyObject) {
+        sendTextUsingContext("water")
+    }
+    
+    
+    //TIMER EXPERIMENT
+    
+    func updateCounter()  {
+        
+        sendIMData()
+        
+    }
+    
+    
 }
 
